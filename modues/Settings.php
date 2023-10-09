39fd68e5d4bfcfb7e931e1213a746feece4a8221
JosephOdhiambo <odhisjoseph85@gmail.com>
2023-10-09 09:18:07 +0300

    Add search functionality to columns that are foreign key within the app

---
+++ modues/Settings.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modues;
+
+/**
+ * settings module definition class
+ */
+class Settings extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modues\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -41,6 +41,9 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Branches', 'url' => ['/branches']],
+            ['label' => 'Companies', 'url' => ['/companies']],
+            ['label' => 'Departments', 'url' => ['/departments']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest
                 ? ['label' => 'Login', 'url' => ['/site/login']]
